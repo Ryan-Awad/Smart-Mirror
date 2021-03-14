868bff3d834df193781f78bb2f27b28e0ab1053c
Ryan StopAskingForMyLastNamePlease <ryanawad@yahoo.com>
2021-03-13 22:28:31 -0500

    Cifar10 TEST for face recog

---
+++ opencv-rasp-config.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y libcblas-dev
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y libhdf5-serial-dev
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libjasper-dev
+sudo apt-get install -y libqtgui4
+sudo apt-get install -y libqt4-test
+sudo apt-get install -y libgtk2.0-dev/testing
