17b5534f3f9b1889ca44b2c8a224eca440cdeb8c
Ryan StopAskingForMyLastNamePlease <ryanawad@yahoo.com>
2021-03-14 23:55:16 -0400

    idk

--- facial_recognition/segmentation.py
+++ facial_recognition/segmentation.py
@@ -4,9 +4,6 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import numpy as np
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
 
 model = keras.Sequential([
     layers.Conv2D(filters=32, kernel_size=(3,3), activation='relu', input_shape=(32,32,3)),
@@ -27,20 +24,12 @@ model.load_weights('cifar10_weights.hdf5')
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+video_capture = cv2.VideoCapture(0)
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-    gray_frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+while True:
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
+    ret, frame = video_capture.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale( # ** PLAY WITH THESE VALUES! **
         image=gray_frame,
@@ -49,28 +38,27 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         minSize=(100, 100)
     )
 
+    cv2.putText(frame, f"FPS: {fps}", (10, 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+
     for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 1)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
         #cv2.putText(frame, 'Face', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
         
         # Getting ROI and preprocessing
-        cropped_img = image[y:y+h,x:x+w] # Crops to Region of Interest (ROI)
+        cropped_img = frame[y:y+h,x:x+w] # Crops to Region of Interest (ROI)
         cropped_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB) # CHANGE TO BGR2GRAY
         cropped_img = cv2.resize(cropped_img, (32,32)) # RESIZE IMG TO (128,128)
         cropped_img = tf.convert_to_tensor(cropped_img, dtype=tf.float32) / 255.0
         cropped_img = tf.expand_dims(cropped_img, axis=0)
         pred = model.predict(cropped_img)
         pred = np.argmax(pred)
-        cv2.putText(image, class_names[pred], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, class_names[pred], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    cv2.imshow('SEGMENTATION CAM', frame)
+
+    exit_key = cv2.waitKey(20) # ESC key 
+    if exit_key == 27:
+        break
 
 video_capture.release()
 cv2.destroyAllWindows()
