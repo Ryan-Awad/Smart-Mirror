5f72a7fdcdb45d5798ca68a5a36dbfb941e4ead1
Ryan StopAskingForMyLastNamePlease <ryanawad@yahoo.com>
2021-04-25 22:23:24 -0400

    Overheating issue fix attemp

--- ml_modules/segmentation.py
+++ ml_modules/segmentation.py
@@ -1,6 +1,11 @@
+# *********** *********************************
+#   !!!! RASPBERRY PI OVERHEATING ISSUE !!!! 
+# *********************************************
+
 import cv2 # KEEP OPENCV VERSION TO 4.5.1.48 FOR THIS TO WORK
 import threading
 import json
+from pyautogui import size
 from subprocess import call
 
 face_cascade = cv2.CascadeClassifier('ml_modules/haarcascade_frontalface_default.xml')
@@ -8,7 +13,10 @@ video_capture = cv2.VideoCapture(0)
 
 face_frame = 0
 faces = ()
+face_label = 'Detected Face'
 face_detected = False
+face_bbox_color = (0, 255, 0)
+width, height = size()
 
 def voice_recog_process():
     call('python3 ml_modules/speech_recog.py', shell=True) # **CHANGE TO 'python3 speech_recog.py' FOR RASPBERRY PI
@@ -25,6 +33,8 @@ while True:
     if face_frame >= detect_secs * fps: # START SPEECH RECOG
         if not voice_recog_thread.is_alive() and not face_detected:
             # LISTENING
+            face_label = 'Listening...'
+            face_bbox_color = (0, 0, 255)
             with open('GUI/data/data.json', mode='r') as data_read:
                 data = json.loads(data_read.readline())
                 data[3] = {
@@ -39,6 +49,8 @@ while True:
         elif not voice_recog_thread.is_alive() and face_detected:
             print('\n[Voice Recognition Thread Ended]')
             voice_recog_thread = threading.Thread(target=voice_recog_process) # Re-define the voice recog thread
+            face_bbox_color = (0, 255, 0)
+            face_label = 'Detected Face'
             face_frame = 0
             face_detected = False
     else: 
@@ -53,3 +65,20 @@ while True:
         minNeighbors=5,
         minSize=(100, 100)
     )
+
+    cv2.putText(frame, f"FPS: {fps}", (10, 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+        
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), face_bbox_color, 1)
+        cv2.putText(frame, face_label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, face_bbox_color, 2)
+
+    cv2.imshow('SEGMENTATION', frame)
+    x, y, w, h = cv2.getWindowImageRect('SEGMENTATION')
+    cv2.moveWindow('SEGMENTATION', int((width/2) - (w/2)), int((height/3) - (h/3)))
+
+    exit_key = cv2.waitKey(20) # ESC key 
+    if exit_key == 27:
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
